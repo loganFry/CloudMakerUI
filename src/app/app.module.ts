38842b52835365451cd1d50f4f5301afac66c905
Logan Fry <fry@Logans-MacBook-Pro.local>
2019-07-03 15:33:24 -0400

    Created team members component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ResourcePanelComponent } from './resource-panel/resource-panel.component';
 import { HeaderComponent } from './header/header.component';
 import { InfoComponent } from './info/info.component';
+import { TeamMembersComponent } from './team-members/team-members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResourcePanelComponent,
     HeaderComponent,
-    InfoComponent
+    InfoComponent,
+    TeamMembersComponent
   ],
   imports: [
     BrowserModule,
