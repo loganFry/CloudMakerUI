539064e769a63519af55650801cf8bc3e303880d
vlad <vldmirea@gmail.com>
2019-07-03 11:49:43 -0400

    Add components for resource panel and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResourcePanelComponent } from './resource-panel/resource-panel.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResourcePanelComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/resource-panel/resource-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-panel',
+  templateUrl: './resource-panel.component.html',
+  styleUrls: ['./resource-panel.component.scss']
+})
+export class ResourcePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
