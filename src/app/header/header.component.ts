a6b664f17c9c439ac43d33370960c650c5cc1d19
vlad <vldmirea@gmail.com>
2019-07-09 16:59:43 -0400

    Recolor header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+      const linkContainer = document.getElementById('header');
+  }
 
   ngOnInit() {
   }
