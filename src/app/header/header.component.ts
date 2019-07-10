2a8c03a088be63707807c9b38a57eeae7a171d93
vlad <vldmirea@gmail.com>
2019-07-10 16:35:32 -0400

    Touch up header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() {
-      const linkContainer = document.getElementById('header');
   }
 
   ngOnInit() {
+      const linkContainer = document.getElementById('header');
+      const btns = linkContainer.getElementsByClassName('link');
+
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < btns.length; i++) {
+          btns[i].addEventListener('click', function() {
+              const current = document.getElementsByClassName('active');
+              current[0].className = current[0].className.replace(' active', '');
+              this.className += ' active';
+          });
+      }
   }
 
 }
