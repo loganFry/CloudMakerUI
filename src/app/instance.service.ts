88f95c70e50410e359fe25db0c384c1d8170223f
Logan Fry <loganfrybball@yahoo.com>
2019-07-18 13:53:05 -0400

    Separating out environment specific config

--- src/app/instance.service.ts
+++ src/app/instance.service.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { GithubFile } from './models/GithubFile';
 import { Instances, Instance } from './models/Instances';
+import { environment } from '../environments/environment';
 
 
 @Injectable({
@@ -12,42 +13,13 @@ import { Instances, Instance } from './models/Instances';
 export class InstanceService {
 
   private apiUrl : string = 'https://api.github.com/';
-  private accessToken : string = 'a3d61d20e9dcba66afc844f95c70fb374cb82a9a';
-  private repoName : string = 'loganFry/CloudMakerUI';
-  private instanceFileName : string = 'instances.json';
-  private instanceFolderName : string = 'Instances/'
 
   constructor(private http: HttpClient) { }
 
   private createHeaders() : HttpHeaders {
     return new HttpHeaders({
-      'Authorization': 'token ' + this.accessToken
+      'Authorization': 'token ' + environment.githubAccessToken
     });
-  }
-
-  getInstanceFile() : Observable<GithubFile> {
-    var customHeaders = this.createHeaders();
-    var options = { headers : customHeaders };
-    return this.http.get<GithubFile>(this.apiUrl + 'repos/' + this.repoName + '/contents/' + this.instanceFileName, options);
-  }
-
-  updateInstanceFile(instances: Instances, sha: string) : Observable<Object> {
-    var customHeaders = this.createHeaders();
-    var options = { headers : customHeaders };
-    // In order to submit file contents to github, we must first convert the
-    // updated instances object to a JSON string, and then encode it in Base64 with btoa()
-    var encodedFileContent: string = btoa(JSON.stringify(instances));
-
-    var body = {
-      "message": "Adding new instance from frontend",
-      "committer": {
-        "name": "Cloudmaker Frontend",
-        "email": "cloudmakerService@gmail.com"
-      },
-      "content": encodedFileContent,
-      "sha": sha
-    };
-    return this.http.put(this.apiUrl + 'repos/' + this.repoName + '/contents/' + this.instanceFileName, body, options);
   }
 
   createNewInstanceFile(instances: Instance[]) : Observable<Object> {
@@ -66,7 +38,9 @@ export class InstanceService {
     };
     var options = { headers: this.createHeaders()};
 
-    return this.http.put(this.apiUrl + 'repos/' + this.repoName + '/contents/' + this.instanceFolderName + fileName, body, options);
+    return this.http.put(this.apiUrl + 'repos/' + environment.instanceRepo + '/contents/' + environment.instanceFolderName + '/' + fileName, 
+      body, 
+      options);
   }
 
   private handleError(error: HttpErrorResponse) {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  instanceRepo: 'loganFry/CloudMakerUI',
+  instanceFolderName: 'FE_Queue',
+  githubAccessToken: 'a3d61d20e9dcba66afc844f95c70fb374cb82a9a'
 };
