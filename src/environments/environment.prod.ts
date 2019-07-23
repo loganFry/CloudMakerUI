559b639d7dd060bb116813e537b053af04f6787c
Logan Fry <loganfrybball@yahoo.com>
2019-07-23 11:34:16 -0400

    Update production access token from Dhanush

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
-//access token is Robbie's
+// new access token was given by dhanush
+// old one from Robbie: bba576a200f03c163dc9462807bc3e2e4d307d03
 
 export const environment = {
   production: true,
   instanceRepo: 'progressive/clo-cloud-maker',
   instanceFolderName: 'FE_Queue',
-  githubAccessToken: 'bba576a200f03c163dc9462807bc3e2e4d307d03',
+  githubAccessToken: '2cec389a68301f32886037deac6a69124b69074f',
   contentUrl: 'https://api.github.com/orgs/progressive/repos/clo-cloud-maker/contents/FE_Queue/',
   comitterName: 'Robbie J Barnes',
   comitterEmail: 'ROBBIE_J_BARNES@progressive.com'
