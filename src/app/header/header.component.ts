14a1688d02f1965e4fbc852eba8e48ebdccdcb0e
Logan Fry <loganfrybball@yahoo.com>
2019-07-25 11:50:07 -0400

    Fixing header responsiveness

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,34 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  currentSelected: string;
+
   constructor() {
   }
 
   ngOnInit() {
-      const linkContainer = document.getElementById('header');
-      const btns = linkContainer.getElementsByClassName('link');
-
-      // tslint:disable-next-line:prefer-for-of
-      for (let i = 0; i < btns.length; i++) {
-          btns[i].addEventListener('click', function() {
-              const current = document.getElementsByClassName('active');
-              current[0].className = current[0].className.replace(' active', '');
-              this.className += ' active';
-              window.scrollBy(0, 700);
-          });
-      }
   }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function
-    myFunction() {
-    console.log("clicked");
-    var x = document.getElementById("header");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+  selectLink(event: KeyboardEvent){
+    this.currentSelected = event.target.innerText;
+  }
 
 }
